4d7f1ce334f9a150b8925239e13d940d6d073f4d
Suzuke <49985092+Suzukeh@users.noreply.github.com>
2021-09-26 10:42:55 +0900

    change sounds from MIDI to tone()

--- e-tin.ino
+++ e-tin.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_BMP085.h>
-#include <MIDI.h>
+//#include <MIDI.h>
 /*
   (1 << 0); // 0000 0000 0000 0001
   (1 << 1); // 0000 0000 0000 0010
@@ -13,7 +13,7 @@
 const unsigned int BIT_FLAG[] = {
     (1 << 0), (1 << 1), (1 << 2), (1 << 3), (1 << 4), (1 << 5)};
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+// MIDI_CREATE_DEFAULT_INSTANCE();
 
 Adafruit_BMP085 bmp;
 // Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
@@ -23,79 +23,64 @@ Adafruit_BMP085 bmp;
 // thats Analog 4 EOC is not used, it signifies an end of conversion XCLR is a
 // reset pin, also not used here
 
-const int notes[] = {
-    74, 76, 78, 79, 81, 83, 84, 85, 86, 88, 90, 91, 93, 95, 96, 97};
+const int freq[] = {294, 330, 370, 392, 440, 494, 523, 554, 587, 659, 698, 740,
+    784, 880, 988, 1047, 1109};
+// const int notes[] = {
+// 74, 76, 78, 79, 81, 83, 84, 85, 86, 88, 90, 91, 93, 95, 96, 97
+//}
+;
 // 0 1 2  3 4 5 6 7  8 9 0  1 2 3 4 5
 // D E F# G A B C C# d e f# g a b c c#
 
 // 穴の識別 上から昇順
-const int sIN[] = {1, 3, 5, 7, 9, 11};
-const int sOUT[] = {2, 4, 6, 8, 10, 12};
+const int sIN[] = {2, 4, 6, 8, 10, 12};
+const int sOUT[] = {3, 5, 7, 9, 11, 15};
 
 const int LED = 13;
 
-const int threshold = 5;
+const double threshold[] = {9, 9, 8, 8, 8, 8};
 
-double pressL, pressH;
+int32_t pressL, pressH;
 
 int prevNote = 20;
 
 void calibrate() {
+    // int32_t pL, pH;
     // 1オクターブ目
     digitalWrite(LED, HIGH);
+    tone(14, 294, 500);
     delay(500);
     digitalWrite(LED, LOW);
     delay(500);
     digitalWrite(LED, HIGH);
-    double pL;
-    delay(300);
-    for (int i = 0; i < 10; i++) {
-        delay(200);
-        pL += bmp.readPressure();
-    }
-    digitalWrite(13, LOW);
-    pressL = pL / 10.0;
+    tone(14, 294, 500);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+    digitalWrite(LED, HIGH);
+    tone(14, 294, 1500);
+    delay(1000);
+    pressL = bmp.readPressure();
+    // Serial.println(pressL);
+    digitalWrite(LED, LOW);
     delay(1000);
     // 2オクターブ目
     digitalWrite(LED, HIGH);
+    tone(14, 587, 500);
     delay(500);
     digitalWrite(LED, LOW);
     delay(500);
     digitalWrite(LED, HIGH);
-    double pH;
-    delay(300);
-    for (int i = 0; i < 10; i++) {
-        delay(200);
-        pH += bmp.readPressure();
-    }
-    pressH = pH / 10.0;
-    digitalWrite(13, LOW);
-}
-
-void setup() {
-    for (int i = 0; i < 6; i++) {
-        pinMode(sIN[i], INPUT);
-        pinMode(sOUT[i], OUTPUT);
-    }
-    for (int i = 0; i < 6; i++) {
-        digitalWrite(sOUT[i], HIGH);
-    }
-
-    pinMode(LED, OUTPUT);
+    tone(14, 587, 500);
+    delay(500);
     digitalWrite(LED, LOW);
-
-    // MIDI
-    MIDI.begin(1);
-
-    // BMP180
-    Serial.begin(9600);
-    if (!bmp.begin()) {
-        Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-        while (1) {
-        }
-    }
-
-    calibrate();
+    delay(500);
+    digitalWrite(LED, HIGH);
+    tone(14, 587, 1500);
+    delay(1000);
+    pressH = bmp.readPressure();
+    // Serial.println(pressH);
+    digitalWrite(LED, LOW);
 }
 
 int checkTimeLag(int a, int b) {
@@ -112,7 +97,7 @@ int readBtn() {
     unsigned int bit = 0;
     for (int i = 0; i < 6; i++) {
         int counter = checkTimeLag(sOUT[i], sIN[i]);
-        if (counter > threshold) {
+        if (counter > threshold[i]) {
             bit |= BIT_FLAG[i];
         }
     }
@@ -128,36 +113,37 @@ void sendnote(unsigned int bit) {
     case 63:
         noteNum = 0;
         break;
-
-    case 62:
+    case 31:
         noteNum = 1;
         break;
-    case 60:
+    case 15:
         noteNum = 2;
         break;
-    case 56:
+    case 7:
         noteNum = 3;
         break;
-    case 48:
+    case 3:
         noteNum = 4;
         break;
-    case 32:
+    case 1:
         noteNum = 5;
         break;
-    case 24:
+    case 6:
         noteNum = 6;
         break;
     case 0:
         noteNum = 7;
         break;
-    case 31:
+    case 62:
         noteNum = 0;
         break;
     default:
         noteNum = 20;
         break;
     }
-    double pressure = bmp.readPressure();
+
+    int32_t pressure = bmp.readPressure();
+    Serial.println(pressure);
     if (pressure > pressH) {
         noteNum += 8;
     } else if (pressure < pressL) {
@@ -165,16 +151,45 @@ void sendnote(unsigned int bit) {
     }
 
     if (prevNote <= 15) {
-        MIDI.sendNoteOff(notes[prevNote], 0, 1);
+        // MIDI.sendNoteOff(notes[prevNote], 0, 1);
     }
     if (noteNum <= 15) {
-        MIDI.sendNoteOn(notes[noteNum], 127, 1);
+        // MIDI.sendNoteOn(notes[noteNum], 127, 1);
+        tone(14, freq[noteNum]);
+        digitalWrite(LED, HIGH);
+    } else {
+        noTone(14);
+    }
+    Serial.println(noteNum);
+    prevNote = noteNum;
+}
+
+void setup() {
+    for (int i = 0; i < 6; i++) {
+        pinMode(sIN[i], INPUT);
+        pinMode(sOUT[i], OUTPUT);
+    }
+    for (int i = 0; i < 6; i++) {
+        digitalWrite(sOUT[i], HIGH);
+    }
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
+    // MIDI
+    // MIDI.begin(1);
+
+    // BMP180
+    Serial.begin(9600);
+    if (!bmp.begin()) {
+        Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+        while (1) {
+        }
     }
 
-    prevNote = noteNum;
+    calibrate();
 }
 
 void loop() {
     unsigned int checkbit = readBtn();
     sendnote(checkbit);
+    delay(10);
 }
