0aa2bf0baee3ff12426c0b879a55bd723b61de6e
Suzuke <49985092+Suzukeh@users.noreply.github.com>
2022-08-26 19:37:57 +0900

    change exports in serial monitor

--- e-tin.ino
+++ e-tin.ino
@@ -1,25 +1,32 @@
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
+#include <BLEMIDI_Transport.h>
+#include <hardware/BLEMIDI_ESP32_NimBLE.h>
+
+BLEMIDI_CREATE_INSTANCE("E-TinWhistle", MIDI)
 
 Adafruit_BMP085 bmp;
 
 void setup() {
   bmp.begin();
   Serial.begin(9600);
-    pinMode(23,OUTPUT);
-
+   pinMode(23,OUTPUT);
+   MIDI.begin();
 }
 
 void loop() {
-  digitalWrite(23,HIGH);
-  Serial.print("温度＝");
-  Serial.print(bmp.readTemperature()); //気温の表示
-  Serial.println(" *C");
-
-  Serial.print("気圧＝");
-  Serial.print(double(bmp.readPressure()) / 100); //気圧(hpa)の表示
-  Serial.println("hPa");
   
-  Serial.println();
-  delay(2000);
+  digitalWrite(23,HIGH);
+  //Serial.print("温度＝");
+  //Serial.print(bmp.readTemperature()); //気温の表示
+  //Serial.println(" *C");
+
+  double press = double(bmp.readPressure()) / 100;
+  //Serial.print(press); //気圧(hpa)の表示
+  String bar="";
+  for (int i = 0; i<((press-1000)*4);i++){
+    bar+="-";
+  }
+  Serial.println(bar);
+  //delay(100);
 }
